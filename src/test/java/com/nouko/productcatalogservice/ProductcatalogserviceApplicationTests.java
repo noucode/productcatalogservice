a633639584ef19931745463d8589dc8fd20005e2
Jean-Claude Nouko <nouko2@hotmail.com>
2026-03-03 23:45:03 -0500

    initial commit

---
+++ src/test/java/com/nouko/productcatalogservice/ProductcatalogserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nouko.productcatalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductcatalogserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
